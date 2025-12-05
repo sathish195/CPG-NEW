ce4240c88fdc1a163e8ad39c4058d8c559a46250
sathish195 <sathishdundigala999@gmail.com>
2025-12-05 12:56:31 +0530

    adding new changes

--- helpers/genCryptoAddress.js
+++ helpers/genCryptoAddress.js
@@ -70,6 +70,19 @@ const address_generate = async (user,chain) => {
     console.log("Address created:", address);
     return address;
     }
+
+    else if (chain === "sepolia") {
+const API_KEY = "3UPEGEFB3A7RZPYSMM587W7E912GFBX29F";
+
+      const sepolia_rpc = `https://sepolia.infura.io/v3/${API_KEY}`;
+      const web3 = new Web3(sepolia_rpc);
+    
+      const account = web3.eth.accounts.create(); 
+      console.log("Sepolia address created:", account.address);
+    
+      return account; // includes address + private key
+    }
+    
      else if (chain === "Ethereum") {
       // var eth_link_check = await rediscon.redisExistSingle("ethlink");
       // if (eth_link_check) {
